9923f4e34c7f46bc7bb1fef27438a6c060e663c7
jalva <jalva@LAPTOP-51CN77K9>
2022-03-02 18:10:13 +0100

    Proyecto AlvarezLechuga2122 comentado con javadoc

--- src/jamon/JamonAlvarezLechugaJL2122.java
+++ src/jamon/JamonAlvarezLechugaJL2122.java
@@ -4,7 +4,7 @@
  */
 package jamon;
 
-/**
+
  /**
 *Clase jamonalvarezlechuga del programa, que define los distintos objetos con unos parametros
 *y realiza acciones de compra y venta de jamones
@@ -12,7 +12,7 @@ package jamon;
  */
 
 public class JamonAlvarezLechugaJL2122 {
-
+//get y set de categoria
     /**
      * @return the categoria
      */
@@ -26,7 +26,7 @@ public class JamonAlvarezLechugaJL2122 {
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
-
+//get y set de precio
     /**
      * @return the precio
      */
@@ -40,7 +40,7 @@ public class JamonAlvarezLechugaJL2122 {
     public void setPrecio(double precio) {
         this.precio = precio;
     }
-
+//get y set de mesesCuracion
     /**
      * @return the mesesCuracion
      */
@@ -54,7 +54,7 @@ public class JamonAlvarezLechugaJL2122 {
     public void setMesesCuracion(int mesesCuracion) {
         this.mesesCuracion = mesesCuracion;
     }
-
+//get y set de stock
     /**
      * @return the stock
      */
@@ -68,58 +68,81 @@ public class JamonAlvarezLechugaJL2122 {
     public void setStock(int stock) {
         this.stock = stock;
     }
+  
+    /** creamos las variables sin inicializar, y de forma privada, 
+     * para que los cambios a estas solo se puedan realizar desde esta clase
+    */
     
     private String categoria;
     private double precio;
     private int mesesCuracion;
     private int stock;
 
-    /* Constructor sin argumentos */
+    /** Constructor sin argumentos
+     * Constructor con parámetro para iniciar todas las propiedades de la clase jamon
+     */
     public JamonAlvarezLechugaJL2122 ()
     {
     }
-    // Constructor con parámetro para iniciar todas las propiedades de la clase
-    // jamon
+  
+   /** aqui es donde inicializas todas las variables de la clase a los del objeto creado en el main
+     */ 
     
-    public JamonAlvarezLechugaJL2122 (String cat, double precio, int stock)
+    public JamonAlvarezLechugaJL2122 (String cat, double precio, int stock) 
     {
         this.categoria =cat;
         this.precio=precio;
         this.stock=stock;
     }
-   // Método para asignar la categoría del jamón
+
+  /** Método para asignar la categoría del jamón   */  
+
     public void asignarCategoria(String cat)
     {
         setCategoria(cat);
     }
-    // Método que me devuelve la categoría del jamón
+    
+    /** Método que me devuelve la categoría del jamón */
+    
     public String obtenerCategoria()
     {
         return getCategoria();
     }
 
-    // Método que me devuelve el stock de jamones disponible en cada momento
+    /** Método que me devuelve el stock de jamones disponible en cada momento */
+    
      public int obtenerStock ()
     {
         return getStock();
     }
 
-    /* Método para comprar jamnones. Modifica el stock.
+     /** Método para comprar jamnones. Modifica el stock.
      * Este método va a ser probado con Junit
-     */
-    public void comprar(int cantidad) throws Exception
+     * comprobamos que no se desee comprar una cantidad ngativa de unidades, si se quiere comprar una cantidad negativa,
+     * mostraremos un mensaje de error, si la cantidad es correcta, gestionaremos el cambio  en la variable de la clase
+     * añadiendole la cantidad deseada.
+      */ 
+    
+     public void comprar(int cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede comprar un nº negativo de jamones");
         setStock(getStock() + cantidad);
     }
 
-    public void vender (int cantidad, java.lang.String paisalvarezlechugajl2122) throws Exception
+   /** funcion la cual entra por parametro la cantidad de unidades a vender y hace los cambios en las variables
+    * de la clase JamonAlvarezLechugaJL2122, si la cantidad que deseas quitar es inferior o igual a 0, no se podra hacer 
+    * y mostraremos un fallo por pantalla, si la cantidad que queremos vender es superior al stock, mostraremos por
+    * pantalla un mensaje de error. Si la cantidad que queremos vender no cumple ninguna de las condiciones anteriores,
+    * se podra hacer la venta. 
+     */
+     
+    public void vender (int cantidad, java.lang.String paisalvarezlechugajl2122) throws Exception 
     {
-        if (cantidad <= 0)
+        if (cantidad <= 0) 
             throw new Exception ("No se puede vender una cantidad negativa de jamones");
         if (obtenerStock()< cantidad)
             throw new Exception ("No  hay suficientes jamones para vender");
-        setStock(getStock() - cantidad);
+        setStock(getStock() - cantidad); 
     }
 }
--- src/jamon/Main.java
+++ src/jamon/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package jamon;
+package jamon; //la clase puede llamar a las funciones de las clases de la carpeta a la que mencionamos, en este caso es la carpeta jamon
 
 /**
 *Clase main del programa, que define un objeto mijamonAlvarezLechugajl2122 con unos parametros
@@ -13,26 +13,39 @@ public class Main {
 
     /**
 * Este metodo es el principal de ejecucion del programa
+* creas el objeto miJamonAlvarezLechugaJL2122 sin inicializar variable que llevara la cuenta de cuantas unidades hay en el stock
+* inicializas el objeto miJamonAlvarezLechugaJL2122 en la clase JamonAlvarezLechugaJL2122, la cual te va a gestionar todos 
+* los cambios que quieras hacer(no te hara cambios en el objeto, el objeto solo sirve para darle valores a las variables de dicha clase)
+* hacemos uso de la funcion vendeJamAlvarezLechugaJL2122() la cual solo mustra informacion por pantalla. 
+* Para hacer uso de esta, debemos introducir el objeto creado anteriormente por parametro.
+* hacemos uso de la funcion compraJamAlvarezLechugaJL2122() la cual solo mustra informacion por pantalla.
+* Para hacer uso de esta, debemos introducir el objeto creado anteriormente por parametro.
+* cogemos la variable inicializada anteriormente (stockActual) y le damos el valor  de stockActual.
+* mostramos por pantalla la variable stockActual, la cual almacenara la cantidad de jamones que hay actualmente en el stock
  */  
 
     public static void main(String[] args) {
-        JamonAlvarezLechugaJL2122 miJamonAlvarezLechugaJL2122;
-        int stockActual;
+        JamonAlvarezLechugaJL2122 miJamonAlvarezLechugaJL2122; 
+        int stockActual; 
         
-        miJamonAlvarezLechugaJL2122 = new JamonAlvarezLechugaJL2122("5Jotas",580,100);
+        miJamonAlvarezLechugaJL2122 = new JamonAlvarezLechugaJL2122("5Jotas",580,100); 
         
-        vendeJamAlvarezLechugaJL2122(miJamonAlvarezLechugaJL2122);
+        vendeJamAlvarezLechugaJL2122(miJamonAlvarezLechugaJL2122); 
         
         compraJamAlvarezLechugaJL2122(miJamonAlvarezLechugaJL2122);
         stockActual = miJamonAlvarezLechugaJL2122.obtenerStock();
         System.out.println("El stock actual es "+ stockActual );
     }
 
-        /**
+    /**
 *Metodo comprajamalvarezlechugajl2122
 * 
 * En este metodo se va a comprar la cantiad de 50 jamones de stock que es de 100 unidades
 * se hace con la idea de que aumentar la variable stock, que pasa a ser de 150 unidades.
+* si salta una excepcion no finalizara el programa, sino, que mostraremos por pantalla el mensaje de error
+* haces uso de la clase JamonAlvarezLechugaJL2122 (la cual gestiona la informacion de miJamonAlvarezLechugaJL2122) 
+* para añadirle 50 unidades. La funcion de dicha clase que te hace el cambio es la funcion comprar, a la cual le metes por 
+* parametro la cantidad que deseas añadirle.
  */  
 
     private static void compraJamAlvarezLechugaJL2122(JamonAlvarezLechugaJL2122 miJamonAlvarezLechugaJL2122) {
@@ -45,19 +58,26 @@ public class Main {
             System.out.print("Fallo al ingresar " + e);
         }
     }
-        /**
+    
+    /**
 *Metodo vendejamalvarezlechugajl2122
 * 
 * En este metodo se va a vender la cantiad de 80 jamones de stock que es de 150 unidades
 * se hace con la idea de que bajar la variable stock, que pasa a ser de 70 unidades.
+* funcion la cual entra una variable por parametro y no devuelve nada.
+* si salta una excepcion no finalizara el programa, sino, que mostraremos por pantalla el mensaje de error
+* haces uso de la clase JamonAlvarezLechugaJL2122 (la cual gestiona la informacion de miJamonAlvarezLechugaJL2122)
+* para quitarle 80 unidades. La funcion de dicha clase que te hace el cambio es la funcion vender, a la cual le metes por
+* parametro la cantidad que deseas quitarle. si hay una excepcion saltara el error que pongamos, 
+* su utilidad es evitar que finalice el programa en caso de error.
  */  
 
-    private static void vendeJamAlvarezLechugaJL2122(JamonAlvarezLechugaJL2122 miJamonAlvarezLechugaJL2122) {
-        try
+    private static void vendeJamAlvarezLechugaJL2122(JamonAlvarezLechugaJL2122 miJamonAlvarezLechugaJL2122) { 
+        try 
         {
             System.out.println("Venta de Jamones");
             miJamonAlvarezLechugaJL2122.vender(80,"España");
-        } catch (Exception e)
+        } catch (Exception e) 
         {
             System.out.print("Fallo al vender " + e);
         }
